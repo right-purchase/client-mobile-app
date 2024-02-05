11daa4502d9b1f6702069017c89a32c0834042a3
Henrique Krausburg Correa <henrique.krausburg.correa@gmail.com>
2024-02-05 14:52:55 -0300

    Linter refactor
    Signed-off-by: Henrique Krausburg Correa <henrique.krausburg.correa@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,11 @@ const styles = StyleSheet.create({
   },
 });
 
-function App() {
+export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      {/* eslint-disable-next-line react/style-prop-object *//* Component prop, not a CSS style */}
       <StatusBar style="auto" />
     </View>
   );
 }
-
-export default App;
