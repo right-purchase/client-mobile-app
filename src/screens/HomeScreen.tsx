484b6d04c89e58581150ac6d4961351e11eddc25
Henrique Krausburg Correa <henrique.krausburg.correa@gmail.com>
2024-02-06 15:26:26 -0300

    Setting HomeScreen style
    Signed-off-by: Henrique Krausburg Correa <henrique.krausburg.correa@gmail.com>

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 import { Text } from "react-native-paper";
 import { Labels } from "../constants/Labels";
+import { View, StyleSheet } from "react-native";
 
 export function HomeScreen() {
-  return <Text>{Labels.RIGHT_PURCHASE}</Text>;
+  return (
+    <View style={styles.container}>
+      <Text variant="displayMedium">{Labels.RIGHT_PURCHASE}</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
